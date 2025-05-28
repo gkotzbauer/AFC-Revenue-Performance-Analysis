ce23ed2f68d089220da98725f031a284730a29a0
Kotz <greg.kotzbauer@gmail.com>
2025-05-28 16:38:40 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
-module.exports = { reactStrictMode: true };
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    esmExternals: true
+  }
+};
+
+module.exports = nextConfig;
